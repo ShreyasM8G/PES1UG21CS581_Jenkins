d8bde60f1acb12fd4dc73746dc36017074d29db4
ShreyasM8G <149608869+ShreyasM8G@users.noreply.github.com>
2024-03-07 18:46:01 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES1UG21CS581 has successfully built and run\n";
+  cout << "This is task 4\n";
   return 0;
 }
